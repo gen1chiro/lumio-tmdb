bb03c16c46d41633b1f210c9f7f984e4784bd0ff
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-06-02 18:19:18 +0800

    feat: initial movie search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import React, { useState, useRef } from "react"
+import { Movie } from "./types/types.ts"
+
 function App() {
+  const [movies, setMovies] = useState<Movie[] | []>([])
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const searchMovies = async (query: string): Promise<Movie[]> => {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`)
+
+      if (!res.ok) throw new Error(`API error: ${res.status}`)
+
+      const data = await res.json()
+      console.log(data)
+      return data.results || []
+    } catch(error) {
+      console.error('Failed to fetch movies:', error);
+      return [];
+    }
+  }
+
+  const handleKeyDown = async (e: React.KeyboardEvent) => {
+    if (e.key !== "Enter") return
+
+    const query = inputRef.current?.value?.trim()
+
+    if (query) {
+      const fetchedMovies: Movie[] = await searchMovies(query)
+      setMovies(fetchedMovies)
+      console.log(movies)
+    }
+  }
 
   return (
-    <div className='bg-red-300 text-7xl'>HELLO</div>
+    <div>
+      <input ref={inputRef}
+             type='text'
+             onKeyDown={handleKeyDown}
+             className='border-gray-300 border-1'
+      />
+    </div>
   )
 }
 
